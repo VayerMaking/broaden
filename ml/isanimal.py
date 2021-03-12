fd66464f0168efb8f563b666d27825a7f2aaf4cc
petardmnv <petar.dmnv@gmail.com>
2021-03-13 00:29:54 +0200

    Add trained model that recognises animals

--- ml/isanimal.py
+++ ml/isanimal.py
@@ -2,38 +2,37 @@ import numpy as np
 import os
 from PIL import Image 
 import tensorflow as tf
-from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.applications import imagenet_utils
 import cv2
 import matplotlib.pyplot as plt
-import time
 import random
-
+import pickle
 
 DIR = "/home/petar/Desktop/broaden/AnimalRecognition/Pictures/"
 
 CATEGORIES = ["Animals", "Others"]
 
-IMAGE_SIZE = 50
+IMAGE_SIZE = 40
 
 training_data = []
 
-def get_training_data():
-	for category in CATEGORIES:
-		path = os.path.join(DIR, category) # Get categories path
-		class_number = CATEGORIES.index(category)
-		for image in os.listdir(path): # Get every image from curr dir
-			try:
-				image_array = cv2.imread(os.path.join(path, image), cv2.IMREAD_GRAYSCALE) # Converting image to grayscale array
-				new_array = cv2.resize(image_array, (IMAGE_SIZE, IMAGE_SIZE)) 
-				training_data.append([new_array, class_number])
-			except Exception as e:
-				print("Image convertion failed!")
+X = []
+# feature set that help to predict the output variable.
+y = []
+# label is simple classification
 
-get_training_data()
-print(len(training_data))
+filepath = ''
+def prepare(filepath):
+	image_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+	new_array = cv2.resize(image_array, (IMAGE_SIZE, IMAGE_SIZE))
+	return new_array.reshape(-1, IMAGE_SIZE, IMAGE_SIZE, 1)
 
-random.shuffle(training_data)# Shuffle all data from training_data.
+model = tf.keras.models.load_model('Animal-CNN.model')
 
-for data in training_data:
-	
+prediction = model.predict([prepare('Pictures/Animals/test1.jpg')])
+
+print(prediction)
+
+
