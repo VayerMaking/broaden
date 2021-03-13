52e36a5a15d06bc399bae442e0507844b3ccab92
VayerMaking <martin.vayer@gmail.com>
2021-03-13 17:47:14 +0200

    added leaderboard endpoints

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request, flash, redirect, url_for, session, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 import time
 from datetime import datetime
 import config
@@ -24,21 +25,32 @@ db = SQLAlchemy(app)
 @dataclass
 class User(db.Model):
     id: int
-    user_id: str
-    username: str
+    #user_id: str
+    #email: str
     points: int
+    verified: bool
+    author_email: str
+    author_id: str
+    name: str
 
     id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.String(40))
-    username = db.Column(db.String(30))
+    #user_id = db.Column(db.String(40))
+    #email = db.Column(db.String(50))
     points = db.Column(db.Integer, default = 0)
+    verified = db.Column(db.Boolean, default = False)
+    author_id = db.Column(db.String(50))
+    author_email = db.Column(db.String(50))
+    name = db.Column(db.String(20))
+
+
+
 
 @dataclass
 class Map(db.Model):
     id: int
     x: int
     y: int
-    count: int
+    count: int          #count of animals in the area(for Nikolcho)
 
     id = db.Column(db.Integer, primary_key = True)
     x = db.Column(db.Integer)
@@ -62,12 +74,22 @@ class News(db.Model):
 @dataclass
 class Upload(db.Model):
     id: int
-    title: str
+    filename: str
     timestamp: datetime
+    lat: str
+    log: str
+    verified: bool
+    author_email: str
+    author_id: str
 
     id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(20))
+    filename = db.Column(db.String(64))
     timestamp = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+    lat = db.Column(db.String(20))
+    log = db.Column(db.String(20))
+    verified = db.Column(db.Boolean, default = False)
+    author_email = db.Column(db.String(50))
+    author_id = db.Column(db.String(30))
 
 @app.route('/', methods = ['GET'])
 def index():
@@ -76,7 +98,7 @@ def index():
 
 @app.route('/user/', methods = ['GET'])
 def user():
-    user = User.query.filter_by(user_id = request.args.get('user_id')).first()
+    user = User.query.filter_by(id = request.args.get('user_id')).first()
     return jsonify(user)
 
 @app.route('/news', methods = ['GET'])
@@ -91,11 +113,55 @@ def send_file(filename):
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
     imagefile = request.files['image']
+    #location = request.args.get('location')
+    lat  = request.form.get('lat')
+    log  = request.form.get('log')
+    author_email  = request.form.get('user_email')
+    author_id  = request.form.get('user_id')
+    print(lat, log)
     filename = werkzeug.utils.secure_filename(imagefile.filename)
     print("\nReceived image File name : " + imagefile.filename)
-    imagefile.save(os.path.join("uploads", filename))
+    file_extension = os.path.splitext(filename)[1]
+    new_filename = random_string(64) #+ file_extension
+    imagefile.save(os.path.join("uploads", new_filename))
+
+    upload = Upload(filename = new_filename, lat = lat, log = log, author_email = author_email, author_id = author_id)
+
+    db.session.add(upload)
+    db.session.commit()
+
     return "Image Uploaded Successfully"
 
+@app.route('/verify', methods = ['GET', 'POST'])
+def verify():
+	uploads = Upload.query.filter_by(verified = False).all()
+	return render_template("verify.html", uploads=uploads)
+
+@app.route('/verify/<path:filename>', methods = ['GET', 'POST'])
+def verify_upload(filename):
+    upload = Upload.query.filter_by(filename = filename).first()
+    upload.verified = True
+    db.session.add(upload)
+    db.session.commit()
+    return redirect('/verify')
+
+@app.route('/set_user_id', methods = ['GET', 'POST'])
+def set_user_id():
+    user_id  = request.form.get('user_id')
+    user = User.query.filter_by(user_id = user_id).first()
+    return jsonify(user)
+
+@app.route('/leaderboard', methods = ['GET', 'POST'])
+def leaderboard():
+    users = User.query.order_by(User.points.desc()).limit(10).all()
+
+    return jsonify(users)
+
+
+def random_string(length):
+    return ''.join(random.choice(string.ascii_letters) for x in range(length))
+
 if __name__ == '__main__':
-	db.create_all()
-	app.run(host="0.0.0.0",port = "5000", debug=True)
+    db.session.execute('pragma foreign_keys=on')
+    db.create_all()
+    app.run(host="0.0.0.0",port = "5000", debug=True)
